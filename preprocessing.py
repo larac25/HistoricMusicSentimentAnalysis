847b2e1a7eb6333fdca7b6dcdc46a2d51a93e021
larac25 <34246312+larac25@users.noreply.github.com>
2021-08-22 20:42:49 +0200

    updated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -10,6 +10,7 @@ from gensim.parsing.preprocessing import strip_punctuation
 from gensim.parsing.preprocessing import strip_non_alphanum
 from gensim.models.word2vec import LineSentence
 from gensim.models.word2vec import PathLineSentences
+from gensim.models import Phrases
 
 
 logging.basicConfig(level=logging.INFO)
@@ -28,12 +29,35 @@ def main():
             # read each document as one big string
             document = open(os.path.join(root, fname)).read()
 
-            prep(document)
-            umlaute(document)
+            # call functions for preprocessing
+            document = umlaute(document)
+            document = prep(document)
+            out_dir = outp_dir()
             # break document into utf8 tokens
             #yield gensim.utils.tokenize(document, lower=True, errors='ignore')
 
-            return document
+            # create filename for preprocessed original file and write processed data
+            prep_file = out_dir + str(os.path.splitext(fname)[0]) + "_prep.txt"
+            with open(prep_file, 'w+') as output:
+                for sentence in document:
+                    output.write(sentence + '\n')
+                    
+        # to do: understand this part!!!
+        sentences = PathLineSentences(out_dir)
+        phrases = Phrases(sentences, min_count=5, threshold=10)
+        # print(list(phrases[sentences]))
+
+        filenames = os.listdir(out_dir)
+        for file in filenames:
+            path = os.path.join(out_dir, file)
+            with open(path, "r+") as f:
+                tokenized_sentences = phrases[LineSentence(path)]
+                f.seek(0)
+                for s in tokenized_sentences:
+                    f.write('{}\n'.format(' '.join(s)))
+                f.truncate()
+
+            # to do: one sentence per line
 
 
 def prep(text):
@@ -49,10 +73,19 @@ def umlaute(text):
     document = document.replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
     document = document.replace('Ä', 'Ae').replace('Ö', 'Oe').replace('Ü', 'Ue')
     document = document.replace('ß', 'ss')
+    document = document.replace('ſ', 's')
     return document
 
 
-logging.info('collected all files in directory')
+def outp_dir():
+    outp_path = '/Users/Lara/Desktop/Uni/Info_4/Masterarbeit/DATA/HMP/anno_corpus/corpus/prep_files/'
+    if not os.path.exists(outp_path):
+        os.makedirs(outp_path)
+
+    return outp_path
+
+
+logging.info('processed all files')
 
 
 if __name__ == '__main__':
